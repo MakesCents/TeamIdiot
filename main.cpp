470a49b7a500d570e7b18204144a1df064570bc9
LittleG09 <soxpride9@comcast.net>
2017-11-18 00:44:07 -0600

    start of file stuff

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <fstream>
+
 #include "Bot.h"
 
 using namespace std;
@@ -6,6 +8,14 @@ using namespace std;
 int main(int argc, char *argv[])
 {
     cout.sync_with_stdio(0);
+    ifstream inFile;
+
+    inFile.open("file.txt");
+    if(!inFile)
+    {
+        cout << "Unable to open file";
+        exit(1);
+    }
 
     Bot bot;
     bot.playGame();
